26d86535240a3d9d835e7bb2380449b34212eedb
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2022-06-08 18:02:27 +0200

    Add project files.

---
+++ EnergyRestApi/Controllers/EnergyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using EnergyRestApi.Managers;
+using EngergyLib;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnergyRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnergyController : ControllerBase
+    {
+        private EnergyManager _manager = new EnergyManager();
+
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet]
+        public ActionResult<List<EnergyData>> GetAll([FromQuery] string value)
+        {
+            List<EnergyData> result = _manager.GetAllData(value);
+            if (result.Count == 0)
+            {
+                return NoContent();
+            }
+            return Ok(result);
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public ActionResult<EnergyData> Post([FromBody] EnergyData newData)
+        {
+            EnergyData createdData = new EnergyData();
+            if (newData.EnergyMetric == null || newData.Value < 0)
+            {
+                return BadRequest();
+            }
+            createdData = _manager.AddCooler(newData);
+            return Created("api/EnergyData/" + createdData.Id, createdData);
+        }
+    }
+}
---
+++ EnergyRestApi/Managers/EnergyManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using EngergyLib;
+
+namespace EnergyRestApi.Managers
+{
+    public class EnergyManager
+    {
+        private static int _nextId = 1;
+
+        private static List<EnergyData> dataList = new List<EnergyData>()
+        {
+            new EnergyData() {Id = _nextId++, EnergyMetric = "joule", Value = 30},
+            new EnergyData() {Id = _nextId++, EnergyMetric = "joule", Value = 10.5},
+            new EnergyData() {Id = _nextId++, EnergyMetric = "calorie", Value = 30.4},
+            new EnergyData() {Id = _nextId++, EnergyMetric = "calorie", Value = 33.2},
+            new EnergyData() {Id = _nextId++, EnergyMetric = "joule", Value = 1.111},
+            
+        };
+
+        public List<EnergyData> GetAllData(string value)
+        {
+            List < EnergyData > tempList = new List<EnergyData>();
+            foreach (var item in dataList)
+            {
+                tempList.Add(new EnergyData
+                {
+                    Id = item.Id,
+                    EnergyMetric = item.EnergyMetric,
+                    Value = item.Value,
+                });
+            }
+
+            if (value == "joule")
+            {
+                foreach (var item in tempList)
+                {
+                    if (item.EnergyMetric == "calorie")
+                    {
+                        item.EnergyMetric = "joule";
+                        item.Value = EnergyConverter.ToJoule(item.Value);
+                    }
+                    
+                }
+            }
+            if (value == "calorie")
+            {
+                foreach (var item in tempList)
+                {
+                    if (item.EnergyMetric == "joule")
+                    {
+                        item.EnergyMetric = "calorie";
+                        item.Value = EnergyConverter.ToCalorie(item.Value);
+                    }
+                }
+            }
+            
+            return tempList;
+        }
+
+        public EnergyData AddCooler(EnergyData data)
+        {
+            data.Id = _nextId++;
+            dataList.Add(data);
+            return data;
+        }
+    }
+}
